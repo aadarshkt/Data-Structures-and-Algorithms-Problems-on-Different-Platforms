dd9d05c9a7e3d4a78c6388b2ddc3549115c1d3b4
aadarshkt <aadarshkt1729@gmail.com>
2023-12-17 11:50:03 +0530

    binary_lifting_practice

--- DSA/Tree/binary_lifting_practice.cpp
+++ DSA/Tree/binary_lifting_practice.cpp
@@ -26,12 +26,79 @@ using namespace std;
 //thus dp[i][j] = dp[dp[i][j-1]][j-1];
 //lower powers of 2 need to be calculated first.
 
+vector<vector<int>> adj;
+vector<vector<int>> dp;
+vector<int> par,vis;
+
+void dfs(int s){
+    vis[s] = 1;
+    for(int x:adj[s]){
+        if(vis[x])continue;
+        par[x] = s;
+        dfs(x);
+    }
+}
+
 int32_t main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    int n,m;cin>>n>>m;
+    adj.resize(n+1);
+    for(int i=0;i<=n;i++)adj[i].clear();
+
+    for(int i=0;i<m;i++){
+        int u,v;cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    int max_jump = ceil(log2(n));
+    //n - binary representation -- next higher bit ka log value represents total bits needed to represent whole number n.
+    //2^(j)>=n solve this, how much j is sufficient to create a value greater than n, take log both sides.
+
+    dp.resize(n+1,vector<int>(max_jump,0));
+
+    //need of parent array
+    par.resize(n+1,-1);
+    //par[i] -> parent of ith node.
+
+    //dfs is need
+    //thus vis is also needed.
     
+    //loop through all nodes for disconnected components
+    for(int i=1;i<=n;i++){
+        if(vis[i])continue;
+        dfs(i);
+    }
+
+    //just jump 2^(0) = 1, you will go to parent.
+    //lower power of 2 calculated, now go for higher
+    for(int i=1;i<=n;i++){
+        dp[i][0] = par[i];
+    }
+
+    for(int j=1;j<31;j++){
+        for(int i=1;i<=n;i++){
+            dp[i][j] = dp[dp[i][j-1]][j-1];
+        }
+    }
+
+    //ready for jumping.
+    int k;cin>>k;
+    int starting_node;cin>>starting_node;
+
+    int curr = starting_node;
+
+    for(int i=0;i<31;i++){
+        if(k&(1<<i)){
+            curr = dp[curr][i];
+        }
+        if(curr==-1)break;//can't jump this amount;
+    }
+    cout<<curr<<'\n';
+
 
     return 0;
 }
